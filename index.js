f8ca79afb5d8c5b987f97ef4c43159b9b98e481f
Lui Amisola <lui.amisola@torodigital.com.au>
2021-02-06 18:00:36 +0800

    update index for big fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Larry Amisola!')
+  res.send('Hello Larry Amisola! Nice to met you')
 });
 
 app.listen(port, () => {
